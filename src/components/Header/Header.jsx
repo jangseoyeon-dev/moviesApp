3ac72fb8e91f6e80e52aaf26dd4b836cde4c06b6
장서연 SKTelecom <tjsus247@naver.com>
2025-04-18 13:33:14 +0900

    Fix: Vercel 버그 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-import styles from "./Header.module.css";
 import { NavLink } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Header.module.css"; // 상대 경로 OK?
 
 const navData = [
   { id: 0, title: "TV", value: "tv" },
