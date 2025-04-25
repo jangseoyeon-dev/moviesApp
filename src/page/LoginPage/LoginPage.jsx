17f125d33f863647c1240ff879acf1b0d13b6db8
장서연 SKTelecom <tjsus247@naver.com>
2025-04-25 10:31:16 +0900

    Feat: 푸터 추가

--- src/page/LoginPage/LoginPage.jsx
+++ src/page/LoginPage/LoginPage.jsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
             />
             <h3 style={{ fontSize: "25px" }}>쿠팡 와우회원 전용</h3>
           </div>
-          <div style={{ fontSize: "60px", lineHeight: "70px" }}>
+          <div className={styles.infoText}>
             <span>최신 영화, TV시리즈,</span>
             <br />
             <span>스포츠 중계를 모두 한 곳에서</span>
@@ -65,7 +65,21 @@ const LoginPage = () => {
           </span>
         </div>
       </div>
-      <footer></footer>
+      <footer className={styles.footer}>
+        <ul>
+          <li>쿠팡(주) | 대표이사: 박대준, 강한승</li>
+          <li>서울시 송파구 송파대로 570</li>
+          <li>사업자 등록번호: 120-88-00767</li>
+          <li>통신판매업신고: 2017-서울송파-0680</li>
+        </ul>
+
+        <ul>
+          <li>고객센터: 1600–9800</li>
+          <li>대표 메일: playrepresent@coupang.com</li>
+          <li>제휴 문의: playbusiness@coupang.com</li>
+          <li>호스팅 서비스 사업자: AWS 코리아</li>
+        </ul>
+      </footer>
     </div>
   );
 };
